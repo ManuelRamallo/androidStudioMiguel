31d2db6e7a2f1902fe19a47caf105882e696cf7b
ManuelRamallo <mramallodiaz96@gmail.com>
2018-01-18 13:46:15 +0100

    creado carpeta games, e insertado proyecto de 3 en rayas

---
+++ 02_Games/01_TresEnRaya/app/src/main/java/com/miguelcr/a01_tresenraya/BoardActivity.java
@@ -0,0 +1,29 @@
+package com.miguelcr.a01_tresenraya;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class BoardActivity extends AppCompatActivity {
+
+    TextView player1, player2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_board);
+
+        player1 = findViewById(R.id.player1);
+        player2 = findViewById(R.id.player2);
+
+        // Rescatar los nombres de los jugadores
+        Bundle extras = getIntent().getExtras();
+
+        String p1Name = extras.getString(Constantes.EXTRA_PLAYER_1);
+        String p2Name = extras.getString(Constantes.EXTRA_PLAYER_2);
+
+        player1.setText(p1Name);
+        player2.setText(p2Name);
+
+    }
+}
