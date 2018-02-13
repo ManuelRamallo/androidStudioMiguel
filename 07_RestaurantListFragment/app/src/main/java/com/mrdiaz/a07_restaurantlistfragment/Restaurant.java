807d143f01145b4a61e589869db3bf2f38c23d8c
ManuelRamallo <mramallodiaz96@gmail.com>
2018-02-13 12:51:43 +0100

    ejercicio Restaurant ListFragment

---
+++ 07_RestaurantListFragment/app/src/main/java/com/mrdiaz/a07_restaurantlistfragment/Restaurant.java
@@ -0,0 +1,8 @@
+package com.mrdiaz.a07_restaurantlistfragment;
+
+/**
+ * Created by mrdiaz on 13/02/2018.
+ */
+
+public class Restaurant {
+}
---
+++ 07_RestaurantListFragment/app/src/main/java/com/mrdiaz/a07_restaurantlistfragment/RestaurantFragment.java
@@ -0,0 +1,49 @@
+package com.mrdiaz.a07_restaurantlistfragment;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.List;
+
+public class RestaurantFragment extends Fragment {
+
+    // TODO: Customize parameters
+    private int mColumnCount = 1;
+    List<Restaurant> restaurantList;
+
+    public RestaurantFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_restaurant_list, container, false);
+
+        // Set the adapter
+        if (view instanceof RecyclerView) {
+            Context context = view.getContext();
+            RecyclerView recyclerView = (RecyclerView) view;
+            if (mColumnCount <= 1) {
+                recyclerView.setLayoutManager(new LinearLayoutManager(context));
+            } else {
+                recyclerView.setLayoutManager(new GridLayoutManager(context, mColumnCount));
+            }
+            recyclerView.setAdapter(new MyRestaurantRecyclerViewAdapter(restaurantList ));
+        }
+        return view;
+    }
+
+}
